9e9d0844d2d12f2c84b0397e7b22ccbd5e0df13c
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-01-17 12:12:40 +0700

    Removed the S3 logic as it was not needed.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, Logger } from '@nestjs/common';
 import { getCurrentInvoke } from '@codegenie/serverless-express';
 import axios from 'axios';
-import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
 
 @Controller()
 export class AppController {
   private readonly logger = new Logger(AppController.name);
-  private readonly s3Client = new S3Client();
 
   @Get('ctx')
   ctx() {
@@ -42,15 +40,6 @@ export class AppController {
       'https://jsonplaceholder.typicode.com/todos/1',
     );
     return await response.json();
-  }
-
-  @Get('listbuckets')
-  async listBuckets() {
-    return await this.s3Client
-      .send(new ListBucketsCommand({}))
-      .then((result) => {
-        result.Buckets?.map((bucket) => bucket.Name);
-      });
   }
 
   @Get('crash')
